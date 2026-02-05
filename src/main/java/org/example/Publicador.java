df20c0e25726ed140dbf083d61e9899cd2d7fcc2
albjinye <albertojinye@gmail.com>
2026-02-05 22:47:59 +0100

    Terminado

--- src/main/java/org/example/Publicador.java
+++ src/main/java/org/example/Publicador.java
@@ -5,7 +5,7 @@ import org.eclipse.paho.client.mqttv3.*;
 public class Publicador {
     public static void main(String[] args) {
 
-        String broker="tcp://test.mosquito.org:1883";
+        String broker="tcp://test.mosquitto.org:1883";
         String topic="/NTT/NTTdata/IBIOL/Spain/Madrid/AlbertoJinYe";
         String message="Mi primer mensaje MQTT --> Alberto Jin Ye";
 
@@ -16,26 +16,26 @@ public class Publicador {
             MqttConnectOptions mqttOptions = new MqttConnectOptions();
 
             mqttOptions.setCleanSession(true);
-            System.out.println("Conectando al broker: " + broker);
+            System.out.println("Conectando al broker soy Publicador: " + broker);
             mqttClient.connect(mqttOptions);
 
             if (mqttClient.isConnected()){
 
-                System.out.println("Conectado");
+                System.out.println("Conectado soy publicador");
 
                 int contador = 0;
                 while (contador <5) {
-
-                    MqttMessage mqttMsg = new MqttMessage(message.getBytes());
+                    String mensajeContado= message + " " + contador;
+                    MqttMessage mqttMsg = new MqttMessage(mensajeContado.getBytes());
                     mqttMsg.setQos(QoS);
 
                     mqttClient.publish(topic, mqttMsg);
                     contador++;
-                    Thread.sleep(5000); // 5 segundos
+                    Thread.sleep(1000); // 5 segundos
                 }
                 mqttClient.disconnect();
                 mqttClient.close();
-                System.out.println("Desconectado");
+                System.out.println("Desconectado publicador");
             }
 
         } catch (MqttException e) {
--- src/main/java/org/example/Suscriptor.java
+++ src/main/java/org/example/Suscriptor.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Suscriptor {
     public static void main(String[] args) {
-        String broker="tcp://test.mosquito.org:1883";
+        String broker="tcp://test.mosquitto.org:1883";
         String topic="/NTT/NTTdata/IBIOL/Spain/Madrid/AlbertoJinYe";
         int QoS=1;
 
@@ -16,20 +16,27 @@ public class Suscriptor {
             MqttConnectOptions mqttOptions = new MqttConnectOptions();
 
             mqttOptions.setCleanSession(true);
-            System.out.println("Conectando al broker: " + broker);
+            System.out.println("Conectando al broker soy Suscriptor: " + broker);
             mqttClient.connect(mqttOptions);
 
 
             if (mqttClient.isConnected()) {
-                System.out.println("Conectado");
+                System.out.println("Conectado soy suscriptor");
 
                 mqttClient.setCallback(new MqttCallback() {
                     @Override
                     public void messageArrived(String topic, MqttMessage message) throws Exception {
 
-                        String mensajeRecibido=message.getPayload().toString();
+                        String mensajeRecibido= new String(message.getPayload());
                         System.out.println(mensajeRecibido);
                         System.out.println();
+
+                        if (mensajeRecibido.equals("Mi primer mensaje MQTT --> Alberto Jin Ye 4")){
+                            System.out.println("Desconectado suscriptor");
+                            mqttClient.disconnect();
+                            mqttClient.close();
+                        }
+
                     }
 
                     @Override
@@ -44,10 +51,7 @@ public class Suscriptor {
                 });
 
                 mqttClient.subscribe(topic, QoS);
-                mqttClient.disconnect();
 
-                mqttClient.close();
-                System.out.println("Desconectado");
             }
         } catch (MqttException e) {
             System.err.println("Error MQTT: " + e.getMessage());
